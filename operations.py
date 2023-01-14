77361a42e5997795c0e902878e325a0cc8640699
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2023-01-14 21:04:29 +0530

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,7 @@
+def integration(function, lower, upper):
+    # function to calculate definite integration
+    pass
+
+def differentiation(function, point):
+    # function to calculate derivative at a point
+    pass
